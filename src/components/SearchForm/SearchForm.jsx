ee7afea06544dc08686882916d70869c5be7e13c
Metaldetect <stas.klymenko@icloud.com>
2023-08-18 14:20:06 +0200

    Create
    Create app but not function

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { FormContainer, Input } from './SearchFormStyles';
+import PropTypes from 'prop-types';
+import debounce from 'lodash.debounce';
+import { searchMoviesByName } from 'Api/Api';
+
+const SearchForm = ({ value, onSearch }) => {
+  const [query, setQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState([null]);
+
+  const handleQueryChange = e => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    debouncedSearch(newQuery);
+  };
+
+  const debouncedSearch = debounce(onSearch, 300);
+
+  useEffect(() => {
+    const fetchMoviesByNameData = async () => {
+      try {
+        const response = await searchMoviesByName(query);
+        setSearchQuery(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMoviesByNameData();
+  }, [query]);
+
+  return (
+    <FormContainer>
+      <Input
+        type="text"
+        placeholder="Search for movies..."
+        value={query}
+        onChange={handleQueryChange}
+      />
+    </FormContainer>
+  );
+};
+
+SearchForm.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};
+
+export default SearchForm;
